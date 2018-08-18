9bc0fdc705447ec8116f310ff4518a5b5e824489
John Mc Kie <johnmckie@HECAN-10m.local>
2018-08-18 17:59:58 -0500

    Added tests for PATCH /task

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -10,7 +10,9 @@ const tasks = [{
   text: 'Sample Task 1o3'
 }, {
   _id: new ObjectID(),
-  text: 'Smaple Task 2o3'
+  text: 'Smaple Task 2o3',
+  completed: true,
+  completedAt: 2101
 }, {
   _id: new ObjectID(),
   text: 'Sample Task 3o3'
@@ -122,10 +124,7 @@ describe('DELETE /tasks/:id', () => {
         SmartTask.findById(hexID).then((task) => {
           expect(task).toNotExist();
           done();
-        //this DOES work :-) !!
         }).catch((e) => done());
-        //this version did not work
-        //}).catch((e) => done(e));
       });
   });
   it('Verification: Expection 404 if the task is not found.', (done) => {
@@ -141,5 +140,46 @@ describe('DELETE /tasks/:id', () => {
       .delete('/tasks/9876543210')
       .expect(404)
       .end(done);
+  });
+});
+
+describe('PATCH /tasks/:id', () => {
+  it('Verification: Ability to update the task', (done) => {
+    var hexID = tasks[0]._id.toHexString();
+    var text = 'This is text';
+
+    request(app)
+      .patch(`/tasks/${hexID}`)
+      .send({
+        completed: true,
+        text
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.task.text).toBe(text);
+        expect(res.body.task.completed).toBe(true);
+        //expect(res.body.task.completedAT).toBe('number');
+        //as of 8/18 running expect 23.5 and the .toBeA function does not work
+      })
+      .end(done)
+  });
+
+  it('Verification: Confirm that completedAT is null if task is incomplete', (done) => {
+    var hexID = tasks[1]._id.toHexString();
+    var text = 'This is text for the second example';
+
+    request(app)
+      .patch(`/tasks/${hexID}`)
+      .send({
+        completed: false,
+        text
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.task.text).toBe(text);
+        //expect(res.body.task.completedAt).toNotExist();
+        //as of 8/18 running expect 23.5 and the .toNotExist function does not work
+      })
+      .end(done)
   });
 });
