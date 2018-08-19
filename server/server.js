9bc5ba881983d6df52c4299e32358b2bbf42c85b
John Mc Kie <johnmckie@HECAN-10m.local>
2018-08-18 19:31:49 -0500

    Add Config and environments

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -8,7 +10,7 @@ var {SmartTask} = require('./models/smarttask');
 var {AuthUser} = require('./models/authuser');
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
